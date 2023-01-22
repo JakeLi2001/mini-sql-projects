a9fbd3a93f8be6ff9381038a48de61964d45157e
JianHui (Jake) Li <LiJake2001@gmail.com>
2023-01-21 21:54:15 -0500

    update sql

--- animal_shelter/code.sql
+++ animal_shelter/code.sql
@@ -45,11 +45,8 @@ SET name = REPLACE(name, '*', '');
 UPDATE animal_outcome
 SET name = REPLACE(name, '*', '');
 
--- might be interesting
-SELECT animal_id, outcome_type, COUNT(outcome_type)
-FROM animal_outcome
-GROUP BY animal_id, outcome_type
-ORDER BY count DESC;
+
+
 -- 1) What are the top 3 most common names for dog and cat? 
 SELECT T.animal_type, T.name, T.count
 FROM (SELECT animal_type, name, COUNT(name), ROW_NUMBER() OVER (PARTITION BY animal_type ORDER BY COUNT(name) DESC) AS name_rank
@@ -67,17 +64,42 @@ GROUP BY breed
 ORDER BY count DESC
 LIMIT 3;
 
--- 3) What are the top 2 common outcome (adoption, transfer, etc.) for dog and cat?
+-- 3) What are the top 2 common outcome (adoption, transfer, etc.) for each animal type?
 SELECT T.animal_type, T.outcome_type, T.count
 FROM (SELECT animal_type, outcome_type, COUNT(outcome_type), ROW_NUMBER() OVER (PARTITION BY animal_type ORDER BY COUNT(outcome_type) DESC) AS country_rank
 	  FROM animal_outcome
-	  WHERE animal_type = 'Cat' OR animal_type = 'Dog'
 	  GROUP BY animal_type, outcome_type) AS T
 where country_rank <= 2
 ORDER BY animal_type;
 
--- 4) What is the average time between intake and adoption for each breed?
+-- 4) Top 10 year and month that has the most intakes.
+SELECT EXTRACT(YEAR FROM date) AS year, EXTRACT(MONTH FROM date) AS month, COUNT(*)
+FROM animal_intake
+GROUP BY year, month
+ORDER BY count DESC
+LIMIT 10;
 
+-- 5) What is the average time between intake and adoption?
+-- First step would be to join the two tables on 'animal_id'. However, there are duplicates in the 'animal_id' column so join would create duplicated records as well. Therefore, I will create Common Table Expressions (CTE) to store the latest unique records by date and then check for duplicates:
+WITH intake_cte AS (
+  SELECT *, ROW_NUMBER() OVER(PARTITION BY animal_id ORDER BY date DESC) AS rn
+  FROM animal_intake), outcome_cte AS (
+  SELECT *, ROW_NUMBER() OVER(PARTITION BY animal_id ORDER BY date DESC) AS rn
+  FROM animal_outcome)
+SELECT animal_id, name, COUNT(*)
+FROM intake_cte -- Or outcome_cte
+WHERE rn = 1
+GROUP BY animal_id, name
+HAVING COUNT(*) > 1;
+-- If no record appears then it means there are no duplicates
 
--- 5) What month has the most intakes?
-
+-- Now we can calcuate the average time between intake and adoption
+WITH intake_cte AS (
+  SELECT *, ROW_NUMBER() OVER(PARTITION BY animal_id ORDER BY date DESC) AS rn
+  FROM animal_intake), outcome_cte AS (
+  SELECT *, ROW_NUMBER() OVER(PARTITION BY animal_id ORDER BY date DESC) AS rn
+  FROM animal_outcome)
+SELECT ROUND(AVG(EXTRACT(DAY FROM (o.date - i.date)))) AS average_days_until_adoption
+FROM intake_cte AS i
+JOIN outcome_cte AS o ON i.animal_id = o.animal_id
+WHERE o.outcome_type = 'Adoption' AND i.rn = 1 AND o.rn = 1;
